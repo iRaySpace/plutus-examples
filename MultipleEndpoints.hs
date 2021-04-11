c6fee0cd482919675f0d79d7cd618e8c686d4d1a
iRaySpace <irayspacii@gmail.com>
2021-04-11 22:58:22 +0800

    docs: useful comments

--- MultipleEndpoints.hs
+++ MultipleEndpoints.hs
@@ -7,13 +7,20 @@ type EndpointSchema =
     BlockchainActions 
         .\/ Endpoint "plus" ()
         .\/ Endpoint "minus" ()
- 
+
+-- @ called as `patterns`, handy way of breaking something up according to pattern
+-- and binding it to names whilst still keeping a reference to the whole thing.
+-- from `Learn you a Haskell`
 plusEndpoint :: Contract () BlockchainActions T.Text ()
 plusEndpoint = logInfo @String "Plus"
 
 minusEndpoint :: Contract () BlockchainActions T.Text ()
 minusEndpoint = logInfo @String "Minus"
 
+-- (>>) sequentially compose two actions
+-- putStr "Hello " >> putStrLn "World" is equivalent to
+-- do putStr "Hello "
+--    putStrLn "World"
 contract :: Contract () EndpointSchema T.Text ()
 contract = plus `select` minus
     where
